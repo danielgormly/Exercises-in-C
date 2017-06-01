3b78463cb868dbddf1da6d7478bf6fd5dcbfdb3e
danielgormly <danielgormly@gmail.com>
2017-06-01 22:28:01 +1000

    Began custom implementation of game of fifteen, removed all CS50 lib references

--- cs50/ps-3/fifteen/fifteen.c
+++ cs50/ps-3/fifteen/fifteen.c
@@ -14,7 +14,6 @@
  
 #define _XOPEN_SOURCE 500
 
-#include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -23,99 +22,65 @@
 #define DIM_MIN 3
 #define DIM_MAX 9
 
-// board
-int board[DIM_MAX][DIM_MAX];
-
-// dimensions
-int d;
-
 // prototypes
-void clear(void);
+void clearScreen(void);
 void greet(void);
-void init(void);
-void draw(void);
-bool move(int tile);
-bool won(void);
+int *initBoard(int boardLength);
+void draw(int *board, int boardLength);
+void logBoard(FILE *file, int *board, int boardLength);
+int get_random(int max);
+int get_int(void);
+int move(int tile);
+int won(void);
 
-int main(int argc, string argv[])
-{
-    // ensure proper usage
-    if (argc != 2)
-    {
-        printf("Usage: fifteen d\n");
+int main(int argc, char *argv[]) {
+    int *board;
+    int boardLength;
+    FILE *file;
+    
+    if (argc != 2) {
+        printf("Usage: fifteen d <int>\n");
         return 1;
     }
 
     // ensure valid dimensions
-    d = atoi(argv[1]);
-    if (d < DIM_MIN || d > DIM_MAX)
-    {
+    boardLength = atoi(argv[1]);
+    if (boardLength < DIM_MIN || boardLength > DIM_MAX) {
         printf("Board must be between %i x %i and %i x %i, inclusive.\n",
             DIM_MIN, DIM_MIN, DIM_MAX, DIM_MAX);
         return 2;
     }
 
     // open log
-    FILE *file = fopen("log.txt", "w");
-    if (file == NULL)
-    {
-        return 3;
-    }
+    file = fopen("log.txt", "w");
+    if (file == NULL) { return 3; }
 
-    // greet user with instructions
     greet();
+    board = initBoard(boardLength);
 
-    // initialize the board
-    init();
-
-    // accept moves until game is won
-    while (true)
-    {
-        // clear the screen
-        clear();
-
-        // draw the current state of the board
-        draw();
-
-        // log the current state of the board (for testing)
-        for (int i = 0; i < d; i++)
-        {
-            for (int j = 0; j < d; j++)
-            {
-                fprintf(file, "%i", board[i][j]);
-                if (j < d - 1)
-                {
-                    fprintf(file, "|");
-                }
-            }
-            fprintf(file, "\n");
-        }
-        fflush(file);
-
+    // game loop
+    while (1) {
+        clearScreen();
+        draw(board, boardLength);
+        logBoard(file, board, boardLength);
         // check for win
-        if (won())
-        {
-            printf("ftw!\n");
+        if (won()) {
+            printf("So easy for you.\n");
             break;
         }
 
-        // prompt for move
-        printf("Tile to move: ");
+        printf("Move tile: ");
         int tile = get_int();
         
         // quit if user inputs 0 (for testing)
-        if (tile == 0)
-        {
-            break;
-        }
+        if (tile == 0) { break; }
 
         // log move (for testing)
         fprintf(file, "%i\n", tile);
         fflush(file);
 
         // move if possible, else report illegality
-        if (!move(tile))
-        {
+        if (!move(tile)) {
             printf("\nIllegal move.\n");
             usleep(500000);
         }
@@ -134,55 +99,76 @@ int main(int argc, string argv[])
 /**
  * Clears screen using ANSI escape sequences.
  */
-void clear(void)
-{
+void clearScreen(void) {
     printf("\033[2J");
     printf("\033[%d;%dH", 0, 0);
 }
 
-/**
- * Greets player.
- */
-void greet(void)
-{
-    clear();
-    printf("WELCOME TO GAME OF FIFTEEN\n");
+void greet(void) {
+    clearScreen();
+    printf("CS50's game of fifteen\nBY DANIEL\n");
     usleep(2000000);
 }
 
-/**
- * Initializes the game's board with tiles numbered 1 through d*d - 1
- * (i.e., fills 2D array with values but does not actually print them).  
- */
-void init(void)
-{
+int *initBoard(int boardLength) {
+    int *board = malloc(sizeof(int) * boardLength + 1);
+    int i, swap;
+    for (i = 0; i < boardLength; i++) board[i] = i;
+    
+    // Fisher-Yates shuffle
+    for (i = boardLength -1; i > 1; i--) {
+        swap = get_random();
+        
+    }
+    return board;
+}
+
+void draw(int *board, int boardLength) {
     // TODO
 }
 
-/**
- * Prints the board in its current state.
- */
-void draw(void)
-{
-    // TODO
+int get_random(int max) {
+    int x;
+    // Skips integers which would otherwise infer a net non-uniform distribution of integers
+    do { x = rand(); }
+    while ((long) x >= max * (int) (((long) RAND_MAX + 1) / (max)));
+    x /= (long) (((long) RAND_MAX + 1) / (max));
+    return x;
 }
 
 /**
  * If tile borders empty space, moves tile and returns true, else
  * returns false. 
  */
-bool move(int tile)
-{
+int move(int tile) {
     // TODO
-    return false;
+    return 0;
 }
 
 /**
  * Returns true if game is won (i.e., board is in winning configuration), 
  * else false.
  */
-bool won(void)
-{
+int won(void) {
     // TODO
-    return false;
+    return 0;
+}
+
+int get_int() {
+    int c = getchar();
+    while(getchar() != '\n' && getchar() != EOF);
+    return c;
+}
+
+void logBoard(FILE *file, int *board, int boardLength) {
+    for (int i = 0; i < boardLength; i++) {
+        for (int j = 0; j < boardLength; j++) {
+            fprintf(file, "%i", board[i + j]);
+            if (j < boardLength - 1) {
+                fprintf(file, "|");
+            }
+        }
+        fprintf(file, "\n");
+    }
+    fflush(file);
 }
