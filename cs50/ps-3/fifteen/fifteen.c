569add566f5ac60d8d7e5230344c32cb18f8fe54
danielgormly <danielgormly@gmail.com>
2017-06-03 21:47:06 +1000

    Completed Game of Fifteen (CS50 week 3)

--- cs50/ps-3/fifteen/fifteen.c
+++ cs50/ps-3/fifteen/fifteen.c
@@ -17,6 +17,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+#include <math.h>
 
 // constants
 #define DIM_MIN 3
@@ -28,14 +30,15 @@ void greet(void);
 int *initBoard(int boardLength);
 void draw(int *board, int boardLength);
 void logBoard(FILE *file, int *board, int boardLength);
+void swap(int *board, int indexA, int indexB);
 int get_random(int max);
 int get_int(void);
-int move(int tile);
-int won(void);
+int move(int *board, int boardLength, int tile);
+int won(int *board, int boardSize);
 
 int main(int argc, char *argv[]) {
     int *board;
-    int boardLength;
+    int boardLength, boardSize;
     FILE *file;
     
     if (argc != 2) {
@@ -50,43 +53,41 @@ int main(int argc, char *argv[]) {
             DIM_MIN, DIM_MIN, DIM_MAX, DIM_MAX);
         return 2;
     }
+    boardSize = boardLength * boardLength;
 
     // open log
     file = fopen("log.txt", "w");
     if (file == NULL) { return 3; }
 
     greet();
-    board = initBoard(boardLength);
+    board = initBoard(boardSize);
 
     // game loop
     while (1) {
         clearScreen();
         draw(board, boardLength);
+
         logBoard(file, board, boardLength);
         // check for win
-        if (won()) {
+        if (won(board, boardSize)) {
             printf("So easy for you.\n");
             break;
         }
-
         printf("Move tile: ");
         int tile = get_int();
-        
-        // quit if user inputs 0 (for testing)
-        if (tile == 0) { break; }
 
         // log move (for testing)
         fprintf(file, "%i\n", tile);
         fflush(file);
 
         // move if possible, else report illegality
-        if (!move(tile)) {
+        if (!move(board, boardLength, tile)) {
             printf("\nIllegal move.\n");
             usleep(500000);
         }
 
         // sleep thread for animation's sake
-        usleep(500000);
+        usleep(200000);
     }
     
     // close log
@@ -107,57 +108,109 @@ void clearScreen(void) {
 void greet(void) {
     clearScreen();
     printf("CS50's game of fifteen\nBY DANIEL\n");
-    usleep(2000000);
 }
 
-int *initBoard(int boardLength) {
-    int *board = malloc(sizeof(int) * boardLength + 1);
-    int i, swap;
-    for (i = 0; i < boardLength; i++) board[i] = i;
-    
+int *initBoard(int boardSize) {
+    int i, randomIndex, tempA, tempB;
+    time_t t;
+
+    int *board = malloc(sizeof(int) * boardSize);
+    for (i = 0; i < boardSize; i++) board[i] = i;
+
+    srand((unsigned) time(&t));
+
     // Fisher-Yates shuffle
-    for (i = boardLength -1; i > 1; i--) {
-        swap = get_random();
-        
+    for (i = boardSize - 1; i > 0; i--) {
+        randomIndex = get_random(i);
+        tempA = board[i];
+        tempB = board[randomIndex];
+        board[randomIndex] = tempA;
+        board[i] = tempB;
     }
     return board;
 }
 
 void draw(int *board, int boardLength) {
-    // TODO
+    int index;
+    for(int i = 0; i < boardLength; i++) {
+        for(int j = 0; j < boardLength; j++) {
+            index = i * boardLength + j;
+            if (board[index] == 0) printf("_");
+            else printf("%i", board[index]);
+            if (board[index] < 10) printf("   ");
+            else printf("  ");
+        }
+        printf("\n");
+    }
 }
 
 int get_random(int max) {
     int x;
+    int limit = RAND_MAX - (RAND_MAX % max);
     // Skips integers which would otherwise infer a net non-uniform distribution of integers
     do { x = rand(); }
-    while ((long) x >= max * (int) (((long) RAND_MAX + 1) / (max)));
-    x /= (long) (((long) RAND_MAX + 1) / (max));
-    return x;
+    while(x >= limit);
+    return x % max;
 }
 
 /**
  * If tile borders empty space, moves tile and returns true, else
  * returns false. 
  */
-int move(int tile) {
-    // TODO
+int move(int *board, int boardLength, int tile) {
+    if (tile == 0) return 0;
+    int diff;
+    int tileIndex = -1;
+    int zeroIndex = -1;
+    int valid = 0;
+    for (int i = 0; i < boardLength * boardLength; i++) {
+        if (board[i] == tile) tileIndex = i;
+        if (board[i] == 0) zeroIndex = i;
+    }
+    printf("tileIndex: %i\n", tileIndex);
+    if (tileIndex == -1) {
+        printf("Couldn't find tile.\n");
+        return 0;
+    }
+    diff = tileIndex - zeroIndex;
+    // behind
+    if (diff == 1 && (tileIndex % boardLength) != 0) valid = 1;
+    // infront
+    if (diff == -1 && (zeroIndex % boardLength) != 0) valid = 1;
+    // above
+    if (abs(diff) == 3) valid = 1;
+    
+    if(valid == 1) {
+        swap(board, tileIndex, zeroIndex);
+        return 1;
+    }
+
     return 0;
 }
 
-/**
- * Returns true if game is won (i.e., board is in winning configuration), 
- * else false.
- */
-int won(void) {
-    // TODO
-    return 0;
+void swap(int *board, int indexA, int indexB) {
+    int tempA, tempB;
+    tempA = board[indexA];
+    tempB = board[indexB];
+    board[indexA] = tempB;
+    board[indexB] = tempA;
+}
+
+int won(int *board, int boardSize) {
+    for(int i = 0; i < boardSize; i++) {
+        if(i != board[i]) return 0;
+    }
+    return 1;
 }
 
 int get_int() {
-    int c = getchar();
-    while(getchar() != '\n' && getchar() != EOF);
-    return c;
+    char str[2], c;
+    char *strCur = str;
+    while((c = getchar()) && c != '\n' && c != EOF) {
+        if (strCur - str < 2) *strCur++ = c;
+    }
+    int x = atoi(str);
+    return x;
 }
 
 void logBoard(FILE *file, int *board, int boardLength) {
